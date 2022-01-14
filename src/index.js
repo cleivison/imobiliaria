0fe2901d386901d2111df643b31d4a32afa338a5
Cleivison <cleivisonfla@hotmail.com>
2022-01-13 22:41:04 -0300

    update 2.0

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-require('dotenv/config');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-
 
 const port = process.env.PORT || 3333;
 app.use(bodyParser.json());
